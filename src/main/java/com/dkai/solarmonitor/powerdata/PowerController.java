832bb55ff276e2c260f318d47e154233fdc12a4c
kai <TurboQP10_github>
2020-04-11 10:22:33 +0200

    Replace wrong path param annotation

--- src/main/java/com/dkai/solarmonitor/powerdata/PowerController.java
+++ src/main/java/com/dkai/solarmonitor/powerdata/PowerController.java
@@ -2,12 +2,11 @@ package com.dkai.solarmonitor.powerdata;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/power")
@@ -17,7 +16,7 @@ public class PowerController {
     private PowerDataService powerDataService;
 
     @GetMapping("/{id}")
-    public PowerData get(@PathParam("id") long id) {
+    public PowerData get(@PathVariable Long id) {
         return powerDataService.read(id);
     }
 
@@ -25,4 +24,5 @@ public class PowerController {
     public PowerData postPowerData(@RequestBody PowerDataDto powerData) {
         return powerDataService.savePowerData(powerData);
     }
+
 }
