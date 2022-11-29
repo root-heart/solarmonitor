948a108cfbd40305fb87c6c4870e85104b49a0f1
kai <kontakt@kaidrahmann.de>
2022-11-29 13:43:27 +0100

    improve charts

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -10,113 +10,111 @@ function loadAndDisplayData(path, displayCallback) {
     request.send();
 }
 
-function drawPowerHistory(powerDataList) {
-    // Something is not working with this sorting, it is completely screwed up. Therefore, we rely on the data to be
-    // returned sorted by the backend (which simply does it the right way)...
-    // powerDataList = powerDataList.sort((a, b) => new Date(a.dateTime).getDate() - new Date(b.dateTime).getDate() );
+function drawHistory(powerDataList) {
     drawBatteryHistory(powerDataList);
+    drawPowerHistory(powerDataList)
 }
 
 function drawBatteryHistory(powerDataList) {
-    Chart.defaults.elements.point = "triangle";
+    let labels = powerDataList.map(powerData => luxon.DateTime.fromISO(powerData.dateTime, {zone: "utc"}).setZone('Europe/Berlin'))
+    let voltages = powerDataList.map(powerData => powerData.batteryVoltage)
+    new Chart(
+        document.getElementById('batteryHistory'), {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Battery Voltage',
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderWidth: 2,
+                    fill: true,
+                    data: voltages,
+                    pointStyle: 'none'
+                }]
+            },
+            options: chartOptions()
+        }
+    );
+}
 
-    let batteryVoltage = [];
-    let solarVoltage = [];
-    let solarPower = [];
-    let loadPower = [];
-    let overallPower = [];
-    let labels = [];
-    let generatedEnergy = [];
-    let usedEnergy = [];
-    let overallEnergy = [];
-    powerDataList.forEach(function (powerData) {
-        let time = luxon.DateTime.fromISO(powerData.dateTime, {zone: "utc"});
-        labels.push(time.setZone('Europe/Berlin'));
-        batteryVoltage.push(powerData.batteryVoltage);
-        solarVoltage.push(powerData.solarVoltage);
-        solarPower.push(powerData.solarPower);
-        loadPower.push(-powerData.loadPower);
-        overallPower.push(powerData.solarPower - powerData.loadPower);
-        generatedEnergy.push(powerData.generatedEnergyToday);
-        usedEnergy.push(-powerData.consumedEnergyToday);
-        overallEnergy.push(powerData.generatedEnergyToday - powerData.consumedEnergyToday);
-    });
+function drawPowerHistory(powerDataList) {
+    let labels = powerDataList.map(powerData => luxon.DateTime.fromISO(powerData.dateTime, {zone: "utc"}).setZone('Europe/Berlin'))
+    let solarPowers = powerDataList.map(powerData => powerData.solarPower)
+    new Chart(
+        document.getElementById('powerHistory'), {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'PV Power',
+                    borderColor: 'rgb(255, 199, 132)',
+                    backgroundColor: 'rgba(255, 199, 132, 0.2)',
+                    fill: true,
+                    borderWidth: 2,
+                    data: solarPowers,
+                    pointStyle: 'none'
+                }]
+            },
+            options: chartOptions()
+        }
+    )
+}
 
-    const powerData = {
-        labels: labels,
-        datasets: [
-            {
-                label: 'Overall Power',
-                borderColor: 'rgb(255, 199, 132)',
-                backgroundColor: 'rgba(255, 199, 132, 0.2)',
-                borderWidth: 2,
-                fill: true,
-                data: overallPower,
-                yAxisID: 'power'
+function drawEnergyHistory(summary) {
+    let labels = summary.dailySummary.map(dailySummary => luxon.DateTime.fromObject({
+        year: dailySummary.year,
+        month: dailySummary.month,
+        day: dailySummary.day
+    }).setZone('Europe/Berlin'))
+    let energies = summary.dailySummary.map(dailySummary => dailySummary.energyThisDay)
+    let options = chartOptions()
+    options.scales.x.time.unit = "day"
+    options.scales.x.time.displayFormats = {day: "dd.MM."}
+    console.log(labels)
+    console.log(energies)
+    new Chart(
+        document.getElementById('energyHistory'), {
+            type: 'bar',
+            data: {
+                labels: labels, datasets: [{
+                    label: 'Collected Energy',
+                    borderColor: 'hsl(205, 20%, 55%)',
+                    backgroundColor: 'hsla(205, 20%, 55%, 0.4)',
+                    fill: true,
+                    borderWidth: 2,
+                    data: energies,
+                    pointStyle: 'none'
+                }]
             },
-            {
-                label: 'Solar Power',
-                borderColor: 'hsl(0, 0%, 35%)',
-                backgroundColor: 'hsl(0, 0%, 35%)',
-                borderWidth: 1,
-                data: solarPower,
-                yAxisID: 'power'
-            },
-            {
+            options: options
+        }
+    )
+}
 
-                label: 'Load Power',
-                borderColor: 'hsl(0, 0%, 35%)',
-                backgroundColor: 'hsl(0, 0%, 35%)',
-                borderWidth: 1,
-                data: loadPower,
-                yAxisID: 'power'
-            },
-            {
-                label: 'Overall Energy',
-                borderColor: 'hsl(205, 20%, 55%)',
-                backgroundColor: 'hsla(205, 20%, 55%, 0.4)',
-                borderWidth: 2,
-                data: overallEnergy,
-                fill: true,
-                yAxisID: 'energy'
-            },
-            {
-                label: 'Generated Energy',
-                borderColor: 'hsl(0, 0%, 35%)',
-                backgroundColor: 'hsl(0, 0%, 35%)',
-                borderWidth: 1,
-                data: generatedEnergy,
-                yAxisID: 'energy'
-            },
-            {
-                label: 'Consumed Energy',
-                borderColor: 'hsl(0, 0%, 35%)',
-                backgroundColor: 'hsl(0, 0%, 35%)',
-                borderWidth: 1,
-                data: usedEnergy,
-                yAxisID: 'energy'
-            },
-            {
-                label: 'Battery Voltage',
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderWidth: 2,
-                data: batteryVoltage,
-                fill: true,
-                yAxisID: 'voltage'
-            }
-        ]
-    };
-
-    const options = {
+function chartOptions() {
+    return {
         datasets: {
             line: {
                 borderJoinStyle: "bevel",
             }
         },
+        elements: {
+            point: {
+                borderWidth: 0,
+                radius: 0,
+                hitRadius: 10,
+                hoverRadius: 10,
+                color: 'rgba(0,0,0,0)',
+                backgroundColor: 'rgba(0,0,0,0)'
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
         animation: false,
-        maintainAspectRatio: false,
-        plugins: {legend: {display: false}},
         scales: {
             x: {
                 display: true,
@@ -130,163 +128,30 @@ function drawBatteryHistory(powerDataList) {
                 grid: {
                     color: 'hsla(0, 0%, 40%, 0.3)',
                     borderColor: 'hsl(0, 0%, 40%)',
+                },
+                ticks: {
+                    // display: false
                 }
             },
-            voltage: {
+            y: {
                 grid: {
                     color: 'hsla(0, 0%, 40%, 0.3)',
                     borderColor: 'hsl(0, 0%, 40%)',
                 },
-                title: {
-                    display: true,
-                    text: 'Battery Voltage'
-                },
-                stack: 'stack',
-                position: 'left'
-                // afterFit: function (axes) {
-                //     axes.width = 69;
-                // },
-            },
-            energy: {
-                grid: {
-                    color: 'hsla(0, 0%, 40%, 0.3)',
-                    borderColor: 'hsl(0, 0%, 40%)',
-                },
-                title: {
-                    display: true,
-                    text: 'Collected (+) / Consumed (-) Energy in kWh'
-                },
-                stack: 'stack',
-                position: 'left',
-                offset: true
-            },
-            power: {
-                grid: {
-                    color: 'hsla(0, 0%, 40%, 0.3)',
-                    borderColor: 'hsl(0, 0%, 40%)',
-                },
-                title: {
-                    display: true,
-                    text: 'PV (+) / Load (-) Power in Watts'
-                },
-                stack: 'stack',
-                position: 'left',
-                offset: true
             }
         }
-    };
-
-    new Chart(
-        document.getElementById('chart'),
-        {
-            type: 'line',
-            data: powerData,
-            options: options
-        }
-    );
+    }
 }
 
 function showPowerDataSummary(powerData) {
     let numberFormat = new Intl.NumberFormat('de-DE', {minimumFractionDigits: 2, maximumFractionDigits: 2});
     document.getElementById("currentPvPower").textContent = numberFormat.format(powerData.solarPower);
-    document.getElementById("currentLoadPower").textContent = numberFormat.format(powerData.loadPower);
-    document.getElementById("currentOverallPower").textContent = numberFormat.format(powerData.solarPower - powerData.loadPower);
-
     document.getElementById("generatedEnergy").textContent = numberFormat.format(powerData.generatedEnergyToday);
-    document.getElementById("consumedEnergy").textContent = numberFormat.format(powerData.consumedEnergyToday);
-    document.getElementById("overallEnergy").textContent = numberFormat.format(powerData.generatedEnergyToday - powerData.consumedEnergyToday);
-
     document.getElementById("currentBatteryVoltage").textContent = numberFormat.format(powerData.batteryVoltage);
     document.getElementById("maximumBatteryVoltage").textContent = numberFormat.format(powerData.maximumBatteryVoltageToday);
     document.getElementById("minimumBatteryVoltage").textContent = numberFormat.format(powerData.minimumBatteryVoltageToday);
 }
 
-function drawSummaryChart(summaryData) {
-    new Chart(document.getElementById("dailySummaryChart"), {
-        type: 'bar',
-        data: {
-            labels: summaryData.dailySummary.map(d => luxon.DateTime.utc(d.year, d.month, d.day)),
-            datasets: [
-                {
-                    label: "energy",
-                    data: summaryData.dailySummary.map(d => d.energyThisDay),
-                    borderColor: 'hsl(205, 20%, 55%)',
-                    backgroundColor: 'hsl(205, 20%, 55%)'
-                }
-            ]
-        },
-        options: {
-            plugins: {legend: {display: false}},
-            scales: {
-                x: {
-                    display: true,
-                    type: 'time',
-                    time: {
-                        unit: 'day',
-                        displayFormats: {
-                            day: 'dd.MM.'
-                        }
-                    },
-                    grid: {
-                        color: 'hsla(0, 0%, 40%, 0.3)',
-                        borderColor: 'hsl(0, 0%, 40%)',
-                    },
-                },
-                y: {
-                    grid: {
-                        color: 'hsla(0, 0%, 40%, 0.3)',
-                        borderColor: 'hsl(0, 0%, 40%)',
-                    },
-                }
-            }
-        }
-    })
-
-    new Chart(document.getElementById("monthlySummaryChart"), {
-        type: 'bar',
-        data: {
-            labels: summaryData.monthlySummary.map(d => luxon.DateTime.utc(d.year, d.month)),
-            datasets: [
-                {
-                    label: "energy",
-                    data: summaryData.monthlySummary.map(d => d.energyThisMonth),
-                    borderColor: 'hsl(205, 20%, 55%)',
-                    backgroundColor: 'hsl(205, 20%, 55%)'
-                }
-            ]
-        },
-        options: {
-            plugins: {legend: {display: false}},
-            scales: {
-                x: {
-                    display: true,
-                    type: 'time',
-                    time: {
-                        unit: 'month',
-                        displayFormats: {
-                            day: 'MMMM'
-                        }
-                    },
-                    ticks: {
-                        maxRotation: 0,
-                    },
-                    grid: {
-                        color: 'hsla(0, 0%, 40%, 0.3)',
-                        borderColor: 'hsl(0, 0%, 40%)',
-                    },
-                },
-                y: {
-                    grid: {
-                        color: 'hsla(0, 0%, 40%, 0.3)',
-                        borderColor: 'hsl(0, 0%, 40%)',
-                    },
-                }
-            }
-        }
-    })
-}
-
-loadAndDisplayData("/powerData/last24Hours", drawPowerHistory);
+loadAndDisplayData("/powerData/last24Hours", drawHistory);
 loadAndDisplayData("/powerData", showPowerDataSummary);
-
-loadAndDisplayData("/powerData/summarized", drawSummaryChart)
+loadAndDisplayData("/powerData/summarized", drawEnergyHistory)
