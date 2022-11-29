0720be4097841b8f009617fe6bc44d395949f9ad
kai <kontakt@kaidrahmann.de>
2022-11-29 11:51:14 +0100

    remove unused code

--- src/main/java/com/dkai/solarmonitor/summary/SummaryDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dkai.solarmonitor.summary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.time.LocalDate;
-
-public interface SummaryDataRepository extends JpaRepository<SummaryData, Long> {
-    SummaryData findByDay(LocalDate day);
-}
